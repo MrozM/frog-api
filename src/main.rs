9e48f7233a565e7343fd2d95d8bae2c49cbf55b9
Maciej Mróz <maciex0325@gmail.com>
2023-12-17 01:20:16 +0100

    remove unsued usings

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
-use mysql::prelude::TextQuery;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -9,10 +8,7 @@ async fn hello() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
+        App::new().service(hello)
     })
-    .bind(("192.168.11.35", 21135))?
-        .run()
-        .await
+    .bind(("192.168.11.35", 21135))?.run().await
 }
